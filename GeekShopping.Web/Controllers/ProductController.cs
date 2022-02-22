ec2fb62a7605f58128da0bd12108a4e09dbe99d0
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2022-02-21 21:20:22 -0300

    Inclusao de views de create e update

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -16,6 +17,45 @@ namespace GeekShopping.Web.Controllers
         {
             var products = await _productService.FindAllProducts();
             return View(products);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+                if(response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var product = _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
         }
     }
 }
